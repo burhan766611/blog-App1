8392780cdc7e5b99ea9ec9d499f2cb99e692e77d
Burhan Sheikh <burhan@Burhans-MacBook-Air.local>
2025-10-03 01:24:24 +0530

    Changes vite

--- Frontend/vite.config.js
+++ Frontend/vite.config.js
@@ -1,10 +1,13 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
+import tailwindcss from '@tailwindcss/vite';
 
 export default defineConfig({
-  plugins: [react()],
+  plugins: [tailwindcss(), react()],
   preview: {
     host: true,
     allowedHosts: ['blog-app1-1-frontend.onrender.com'],
   },
 });
+
+
